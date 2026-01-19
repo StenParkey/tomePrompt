182c211be8249606f86cb6a15ca54e0733e3a593
Jacky <jpstennes@gmail.com>
2026-01-19 12:28:53 -0700

    Added comment about resources used to make ASCII Art

--- dir/models/assetsObject.js
+++ dir/models/assetsObject.js
@@ -1,3 +1,6 @@
+// ASCII Art made with tools from the ASCII Art Archive at asciiart.eu
+// Title made using the text to ascii art tool {font: 'The Edge', Width: 116, Border: 'Bold Block Frame', V. Padding: 1}
+
 const assetObject = {
 	title: `
 		███████████████████████████████████████████████████████████████████████

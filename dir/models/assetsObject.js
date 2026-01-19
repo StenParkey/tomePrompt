8f34426686017913df5bfefb4f6c864ad6a037e1
Jacky <jpstennes@gmail.com>
2026-01-19 16:43:49 -0700

    Fixed default variable name to align with file name

--- dir/models/assetsObject.js
+++ dir/models/assetsObject.js
@@ -1,7 +1,7 @@
 // ASCII Art made with tools from the ASCII Art Archive at asciiart.eu
 // Title made using the text to ascii art tool {font: 'The Edge', Width: 116, Border: 'Bold Block Frame', V. Padding: 1}
 
-const assetObject = {
+const assetsObject = {
 	title: `
 		███████████████████████████████████████████████████████████████████████
 		█▌                                                                   ▐█
@@ -17,4 +17,4 @@ const assetObject = {
 	splashFrames: []
 }
 
-export default { assetObject };
+export default { assetsObject };

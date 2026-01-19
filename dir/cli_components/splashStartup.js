0142640acde1fc4f2fb4114a1a028ce71c621b55
Jacky <jpstennes@gmail.com>
2026-01-19 16:53:23 -0700

    Staged out splash startup screen component

---
+++ dir/cli_components/splashStartup.js
@@ -0,0 +1,8 @@
+import cliFrames from 'cli-frames';
+import assets from '../models/assetsObject.js';
+
+export default async function splashStartup() {
+	// The top of this function will be reserved for running the splash animation, then clearing screen and showing the title
+
+	console.log(assets.assetsObject.title)
+}

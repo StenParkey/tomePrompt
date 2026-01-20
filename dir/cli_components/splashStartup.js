84e0ba20241c41b28565273d7116be7bb3e3d5e5
Jacky <jpstennes@gmail.com>
2026-01-19 17:04:39 -0700

    added line break after title art

--- dir/cli_components/splashStartup.js
+++ dir/cli_components/splashStartup.js
@@ -4,5 +4,6 @@ import assets from '../models/assetsObject.js';
 export default async function splashStartup() {
 	// The top of this function will be reserved for running the splash animation, then clearing screen and showing the title
 
-	console.log(assets.assetsObject.title)
-}
+	console.log(assets.assetsObject.title);
+	console.log('\n');
+};
